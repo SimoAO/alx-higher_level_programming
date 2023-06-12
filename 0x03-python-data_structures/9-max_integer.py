53824cf5fe5e44ce45fd5e23ab8fa992ad8dd074
simoao <aitouaazizmohammed@gmail.com>
2023-06-12 15:00:59 -0700

    task5,...,10

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        maxi = my_list[0]
+        for i in my_list[1:]:
+            if (i > maxi):
+                maxi = i
+        return (maxi)
