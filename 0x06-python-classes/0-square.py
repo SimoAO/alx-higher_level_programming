0a53d3c92b4c4a2a59d36f71879436ac85ca845e
simoao <aitouaazizmohammed@gmail.com>
2023-06-27 14:05:42 -0700

    task0,1,2

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""the square module"""
+class Square:
+    """defines a square"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""the square module"""
+class Square:
+    """defines a square"""
+    def __init__(self, size):
+        self.__size = size
