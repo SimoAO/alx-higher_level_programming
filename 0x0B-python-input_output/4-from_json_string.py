7adfc571e765119c843b860f5ad88235a39c5ea8
simoao <aitouaazizmohammed@gmail.com>
2023-07-11 07:34:12 -0700

    task2,3,4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""The from_json_string module"""
+import json
+
+
+def from_json_string(my_str):
+    """a function that returns an object
+    represented by a JSON string
+    """
+    return json.loads(my_str)
