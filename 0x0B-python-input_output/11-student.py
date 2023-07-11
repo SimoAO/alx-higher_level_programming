be9ba0405328d3687048e47134e353f0532c1621
simoao <aitouaazizmohammed@gmail.com>
2023-07-11 15:37:29 -0700

    task11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""the student module"""
+
+
+class Student:
+     """a class Student that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """initialize a student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student"""
+        if type(attrs) is list and all(type(i) is str for i in attrs):
+            dic = {}
+            for j in attrs:
+                if j in self.__dict__:
+                    dic[j] = self.__dict__[j]
+            return dic
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            self.__dict__[key] = value
