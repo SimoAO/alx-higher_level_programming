ab46fa3be914f30b805dc2752c430ccef6595730
SimoAO <aitouaazizmohammed@gmail.com>
2023-09-15 12:04:59 -0700

    task1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""The Base class"""
+
+
+import json
+
+
+class Base:
+    """A class named Base"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initiliazes an instance"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
