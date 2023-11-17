9e7f1b414c022846e6d9bdd050a47651f686d6e3
simoao <aitouaazizmohammed@gmail.com>
2023-11-17 23:26:37 +0100

    task5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A module that takes in the name of a state as an argument and lists
+all cities of that state from a database.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(
+            host="localhost", port=3306, user=sys.argv[1],
+            password=sys.argv[2], database=sys.argv[3])
+
+    dbcur = db.cursor()
+
+    dbcur.execute("SELECT cities.name FROM cities INNER JOIN states \
+            ON cities.state_id = states.id WHERE states.name LIKE \
+            BINARY %s ORDER BY cities.id ASC", (argv[4],))
+
+    for city in (dbcur.fetchall()):
+        print(", ".join(city[0]))
+
+    dbcur.close()
+    db.close()
