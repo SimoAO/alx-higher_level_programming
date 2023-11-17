a804ccd06cb5f7585accd68d2b6db613f17b2e11
simoao <aitouaazizmohammed@gmail.com>
2023-11-17 23:31:35 +0100

    task5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     dbcur.execute("SELECT cities.name FROM cities INNER JOIN states \
             ON cities.state_id = states.id WHERE states.name LIKE \
-            BINARY %s ORDER BY cities.id ASC", (argv[4],))
+            BINARY %s ORDER BY cities.id ASC", (argv[4], ))
 
     for city in (dbcur.fetchall()):
         print(", ".join(city[0]))
