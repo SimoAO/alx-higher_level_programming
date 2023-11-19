0562bc2be32c9d8400c511b68c086c31bb08a47d
simoao <aitouaazizmohammed@gmail.com>
2023-11-19 23:17:42 +0100

    task10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""A module that prints the first State object from the database"""
+"""
+A module that prints the State object with the name passed as
+argument from the database
+"""
 
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -17,8 +20,7 @@ if __name__ == "__main__":
     session = Session()
 
     pstate = session.query(State).\
-            filter(State.name.like(sys.argv[4])).first().\
-            order_by(State.id)
+            filter(State.name.like(sys.argv[4])).first()
     if pstate:
         print("{}".format(pstate.id))
     else:
