7418f31939b38c32f4b38cfe2982b58ec7d0f1ff
simoao <aitouaazizmohammed@gmail.com>
2023-11-20 01:07:58 +0100

    task11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -3,10 +3,10 @@
 A module that adds the State object "Louisiana" to the database
 """
 
+import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import sys
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
