95d8bdd0f9b3041900379a3b117963dfc2b661c5
SimoAO <aitouaazizmohammed@gmail.com>
2023-11-30 07:19:05 -0800

    task3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes in a URL and displays all HTTP methodsthe server accept#!/bin/bash
-curl -i -s "$1" | grep -i "Allow" | cut -d ' ' -f 2-
+# Takes in a URL and displays all HTTP methodsthe server accept
+curl -sI "$1" | grep -i "Allow" | cut -d ' ' -f 2-
