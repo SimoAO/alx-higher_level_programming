65e80deccf9feb9edcd16647b5a5e1709c8cf75e
simoao <aitouaazizmohammed@gmail.com>
2024-01-04 00:50:35 +0100

    task

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const fp = process.argv[2];
 const cont = process.argv[3];
-fs.writeFile(fp, cont, 'utf8', (err) {
+fs.writeFile(fp, cont, 'utf8', (err) => {
   if (err) {
     console.log(err);
   }
